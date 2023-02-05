54231ea1e4a98edf6cb1004259af294d9053ee27
thaker0503 <thaker.yatharth.05@gmail.com>
2023-02-05 16:29:38 +0530

    raycaster update

--- three-23-raycaster-and-mouse-events/src/script.js
+++ three-23-raycaster-and-mouse-events/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import Stats from 'three/addons/libs/stats.module.js';
 
 /**
  * Base
@@ -35,6 +36,23 @@ const object3 = new THREE.Mesh(
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
+
+
+/**
+ * Raycaster
+ */
+
+let mouse = {}
+
+window.addEventListener('mousemove', (e) => {
+    mouse.x = e.clientX / sizes.width * 2 - 1
+    mouse.y = - (e.clientY / sizes.height * 2 - 1) 
+
+})
+
+const raycaster = new THREE.Raycaster()
+
+
 
 /**
  * Sizes
@@ -80,6 +98,9 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+// const stats = new Stats()
+
+
 /**
  * Animate
  */
@@ -88,6 +109,34 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // const rayOrigin = new THREE.Vector3(-3,0,0)
+    // const rayDirection = new THREE.Vector3(1,0,0)
+    // rayDirection.normalize()
+
+    // raycaster.set(rayOrigin,rayDirection)
+
+    object1.position.y = Math.sin(elapsedTime * 0.3)
+    object2.position.y = Math.sin(elapsedTime * 0.7)
+    object3.position.y = Math.sin(elapsedTime * 1.2)
+
+    raycaster.setFromCamera( mouse, camera );
+
+    const objects = [object1,object2,object3]
+
+    const intersects = raycaster.intersectObjects(objects)
+
+    for (const obj of objects) {
+        obj.material.color.set(0xff0000)
+    }
+
+    if(intersects.length){
+        intersects[0].object.material.color.set(0x0000ff)
+    }
+    // for (const obj of intersects) {
+    // }
+    
+    
 
     // Update controls
     controls.update()
