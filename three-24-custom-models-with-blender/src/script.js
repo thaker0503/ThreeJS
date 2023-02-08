0944923b4f62a0514d6a916a62275694dc65bc47
thaker0503 <thaker.yatharth.05@gmail.com>
2023-02-09 00:00:42 +0530

    Complete Lesson 24

--- three-24-custom-models-with-blender/src/script.js
+++ three-24-custom-models-with-blender/src/script.js
@@ -28,7 +28,7 @@ gltfLoader.setDRACOLoader(dracoLoader)
 let mixer = null
 
 gltfLoader.load(
-    '/models/hamburger.glb',
+    '/models/hamBurger.glb',
     (gltf) =>
     {
         scene.add(gltf.scene)
